faded3b3e656d435179a8aa2f756cd8eb7b85a08
danieaneta <83565907+danieaneta@users.noreply.github.com>
2023-03-07 20:17:59 -0600

    Update loop_programs.cs

--- console_apps/loop_programs.cs
+++ console_apps/loop_programs.cs
@@ -1,3 +1,11 @@
+public class oneToTen {
+    public static void main(String []args) {
+        for (int i = 0; i <= 10; i++)
+        System.out.println(i);
+    }
+}
+
+
 public class Nested {
     public static void main(String []args) {
         for (int i = 1; i <= 3; i++) {
